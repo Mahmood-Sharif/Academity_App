3ef62d2493badba02fc7d2cec3428573b15359a3
Mahmood-Sharif <ma7moodfareed@hotmail.com>
2024-03-06 16:41:55 +0300

    UI design home screen

---
+++ academity_app/lib/views/widgets/nav_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onItemSelected;
+
+  const CustomBottomNavBar({
+    Key? key,
+    required this.selectedIndex,
+    required this.onItemSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: <BottomNavigationBarItem>[
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.home_outlined),
+          label: 'Home',
+          activeIcon: Icon(Icons.home),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.shield_outlined),
+          label: 'Academity',
+          activeIcon: Icon(Icons.shield_rounded),
+        ),
+     BottomNavigationBarItem(
+          icon: Container(
+            width: 48, // Increase the width for a larger circle
+            height: 48, // Increase the height for a larger circle
+            decoration: const BoxDecoration(
+              color: Color(0xffff3200),
+              shape: BoxShape.circle,
+            ),
+            child: const Icon(Icons.qr_code_scanner, color: Colors.white),
+          ),
+          label: '',
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.calendar_today_outlined),
+          label: 'Schedule',
+          activeIcon: Icon(Icons.calendar_today),
+        ),
+        const BottomNavigationBarItem(
+          icon: Icon(Icons.person_outline),
+          label: 'Profile',
+          activeIcon: Icon(Icons.person),
+        ),
+      ],
+      currentIndex: selectedIndex,
+      selectedItemColor:const Color(0xFF8B0000),
+      onTap: onItemSelected,
+    );
+  }
+}
