fd17e466c9854c79e317520cb5533f05069ac10c
Mahmood-Sharif <ma7moodfareed@hotmail.com>
2024-03-10 15:04:15 +0300

    latest features

--- app/Entities/ClassEntity.php
+++ app/Entities/ClassEntity.php
@@ -4,12 +4,16 @@ namespace App\Entities;
 
 use CodeIgniter\Entity\Entity;
 
+namespace App\Entities;
+
+use CodeIgniter\Entity\Entity;
+
 class ClassEntity extends Entity
 {
     protected $casts = [
-      'class_id'   => 'integer',
-      'min_age'    => 'integer',
-      'max_age'    => 'integer',
-      'academy_id' => 'integer',
+        'class_id'   => 'integer',
+        'min_age'    => 'integer',
+        'max_age'    => 'integer',
+        'academy_id' => 'integer',
     ];
 }
