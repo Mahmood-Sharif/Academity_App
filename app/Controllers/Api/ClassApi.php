e5c4f868a34cb816137327cb27acb62efd43a07b
Mahmood-Sharif <ma7moodfareed@hotmail.com>
2024-03-30 22:09:20 +0300

    registeration to academy

--- app/Controllers/Api/ClassApi.php
+++ app/Controllers/Api/ClassApi.php
@@ -7,6 +7,7 @@ namespace App\Controllers\Api;
 use CodeIgniter\RESTful\ResourceController;
 use App\Models\ClassModel;
 use App\Models\ClassTimingModel;
+use App\Models\EnrollmentModel;
 
 class ClassApi extends ResourceController
 {
@@ -29,5 +30,24 @@ class ClassApi extends ResourceController
         $classWithPrice->timings = $timings;
 
         return $this->respond($classWithPrice);
+    }
+
+    public function enrollWithCode()
+    {
+        $studentId = auth()->id(); // get user id from token, requires the token filter to be enabled
+        $regCode = $this->request->getPost('regCode');
+
+        if (!$studentId || !$regCode) {
+            return $this->fail('Missing student ID or registration code');
+        }
+
+        $enrollmentModel = new EnrollmentModel();
+        $enrolled = $enrollmentModel->enrolWithCode($studentId, (string)$regCode);
+
+        if (!$enrolled) {
+            return $this->fail('Enrollment failed. Class may not exist or be full.');
+        }
+
+        return $this->respondCreated('Student successfully enrolled.');
     }
 }
