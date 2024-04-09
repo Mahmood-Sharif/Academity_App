46fa73a09cf1507e3661aaacaf645cfd90168abd
Salman-Almarzouqi <202000265@student.polytechnic.bh>
2024-04-10 00:02:43 +0300

    coach localization

--- academity_app/lib/services/academity_api.dart
+++ academity_app/lib/services/academity_api.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:academity_app/.env.dart';
+import 'package:academity_app/env.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
