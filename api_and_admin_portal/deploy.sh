06ee4fc718793cb38eeb361940acf12e323b7243
Naofal <naofal@naofal.xyz>
2024-04-18 00:22:05 +0300

    add deployment and startup scripts

---
+++ api_and_admin_portal/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -eux
+
+if [ "$(basename "$(pwd)")" = 'api_and_admin_portal' ]; then
+	cd ..
+fi
+
+git subtree split --prefix=api_and_admin_portal --branch=server-deploy admin-portal
+git push azure server-deploy:master
