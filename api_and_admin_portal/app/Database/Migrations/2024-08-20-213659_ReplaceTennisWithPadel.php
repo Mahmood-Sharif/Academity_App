d78b662e9765cc6f16e08d09019a0a18f0635462
Mahmood-Sharif <ma7moodfareed@hotmail.com>
2024-08-21 00:37:46 +0300

    added correct migration for padel

--- api_and_admin_portal/app/Database/Migrations/2024-08-20-213659_ReplaceTennisWithPadel.php
+++ api_and_admin_portal/app/Database/Migrations/2024-08-20-213659_ReplaceTennisWithPadel.php
@@ -4,12 +4,13 @@ namespace App\Database\Migrations;
 
 use CodeIgniter\Database\Migration;
 
-class FixSwimmingAndPadelNames extends Migration
+class ReplaceTennisWithPadel extends Migration
 {
     public function up()
     {
         $this->db->table('sports')->update(['name' => 'Swimming'], ['sport_id' => 3]);
         $this->db->table('sports')->update(['name' => 'Padel'], ['sport_id' => 6]);
+        $this->db->table('media')->update(['url' => 'images/Padel.jpg'], ['media_id' => 6]);
     }
 
     public function down()
