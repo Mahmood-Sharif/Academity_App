f3ab5c896a8dd78df6d385295ace864cfa147a78
Mahmood-Sharif <ma7moodfareed@hotmail.com>
2024-08-21 00:39:13 +0300

    fixed migration

--- app/Database/Migrations/2024-08-20-213659_ReplaceTennisWithPadel.php
+++ app/Database/Migrations/2024-08-20-213659_ReplaceTennisWithPadel.php
@@ -8,7 +8,6 @@ class ReplaceTennisWithPadel extends Migration
 {
     public function up()
     {
-        $this->db->table('sports')->update(['name' => 'Swimming'], ['sport_id' => 3]);
         $this->db->table('sports')->update(['name' => 'Padel'], ['sport_id' => 6]);
         $this->db->table('media')->update(['url' => 'images/Padel.jpg'], ['media_id' => 6]);
     }
